ac358411b0ec3407d59b1ef0dee68caebd7d2616
ramapat <ramapat111@yandex.com>
2015-03-10 12:51:59 +0200

    login_required part removed

--- demo/routers.py
+++ demo/routers.py
@@ -2,7 +2,6 @@ from swampdragon import route_handler
 from swampdragon.route_handler import ModelPubRouter
 from .models import Notification
 from .serializers import NotificationSerializer
-from django.contrib.auth.decorators import login_required
 
 
 class NotificationRouter(ModelPubRouter):
@@ -10,10 +9,10 @@ class NotificationRouter(ModelPubRouter):
     route_name = 'notifications'
     model = Notification
     serializer_class = NotificationSerializer
-    
-    @login_required
-    def subscribe(self, **kwargs):
-        super().subscribe(**kwargs)
+
+    #@login_required
+    #def subscribe(self, **kwargs):
+    #    super().subscribe(**kwargs)
 
     def get_subscription_contexts(self, **kwargs):
         return {'user_id': self.connection.user.pk}
